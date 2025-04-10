0f1199ad68f3cacb5850ce793bf35cca60f23072
Luis Felipe Prada Ramirez <lfprada30@gmail.com>
2025-04-10 17:09:48 -0400

    Update ejercicio2.py

--- AUXILIATURA/INTRODUCCION A POO/Ejercicio2/codigos/ejercicio2.py
+++ AUXILIATURA/INTRODUCCION A POO/Ejercicio2/codigos/ejercicio2.py
@@ -1,1 +1,32 @@
 
+class Empleado:
+    def __init__(self, nombre, sueldo):
+        self.nombre = nombre
+        self.sueldo = sueldo
+    
+    def sueldoanual(self):
+        return self.sueldo * 12
+    
+    def aplicaraumento(self):
+        self.sueldo *= 1.10
+    
+
+    def mostrardatos(self):
+        print(f"Empleado: {self.nombre}")
+        print(f"Sueldo mensual: ${self.sueldo:.2f}")
+        print(f"Sueldo anual: ${self.sueldoanual():.2f}")
+        print("------------------------")
+
+empleado1 = Empleado("Luis Prada", 2500)
+empleado2 = Empleado("Alejandro Altamirano", 3200)
+
+print("=== Antes del aumento ===")
+empleado1.mostrardatos()
+empleado2.mostrardatos()
+
+empleado1.aplicaraumento()
+empleado2.aplicaraumento()
+
+print("\n=== Después del aumento ===")
+empleado1.mostrardatos()
+empleado2.mostrardatos()
